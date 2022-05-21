ab825789f72f53ad5fb6319ce21adfb62f7d2ecd
Daniel <danieldondorp@gmail.com>
2022-05-21 19:18:01 +0200

    added user control over the time window to plot

--- streamlit_dashboard/main.py
+++ streamlit_dashboard/main.py
@@ -26,19 +26,23 @@ container2 = st.empty()
 db = connector.connect(host=SQL_HOST, user=USERNAME, password=PASSWORD, database=DATABASE)
 sensors = pd.read_sql("SELECT * FROM sensors;", db)
 
-def load_data():
+def load_data(n_hours):
     df = pd.read_sql(f"SELECT s.name, m.measured_at, m.temperature, m.humidity "
                      f"FROM measurements m "
                      f"JOIN sensors s "
                      f"ON s.id = m.sensor_id "
-                     f"WHERE m.measured_at >= NOW() - INTERVAL 2 DAY;", db)
+                     f"WHERE m.measured_at >= NOW() - INTERVAL {n_hours} HOUR;", db)
     db.commit()
     return df
 
+n_hours = st.number_input(label = "Show data for hours:", min_value = 1, max_value =7*24, value = 24)
 while True:
-    data = load_data()
+
     with container.container():
         st.write(time.strftime("%H:%M"))
+
+
+    data = load_data(n_hours)
     with container1.container():
         columns = st.columns(len(sensors))
         for col, s in zip(columns, sensors.name.values):
@@ -61,12 +65,11 @@ while True:
     with container2.container():
         fig_cols = st.columns([1,1])
         with fig_cols[0]:
-            fig = px.line(data, x = "measured_at", y = "temperature", color = "name", title = "Temperature last 24h")
+            fig = px.line(data, x = "measured_at", y = "temperature", color = "name", title = f"Temperature last {n_hours}h")
             st.write(fig)
         with fig_cols[1]:
-            fig = px.line(data, x = "measured_at", y = "humidity", color = "name", title = "Humidity last 24h")
+            fig = px.line(data, x = "measured_at", y = "humidity", color = "name", title = f"Humidity last {n_hours}h")
             st.write(fig)
-        # plt.close(fig)
 
     time.sleep(10)
 
